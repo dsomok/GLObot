887d8d780207bdc02a9a874569107468081dea52
Alexander Nalbandov <Alexander.Nalbandov@outlook.com>
2018-06-12 21:09:55 +0300

    Updated script to generate certificate

--- Nginx/entrypoint.sh
+++ Nginx/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:2048 -out server.key
-openssl req -new -key server.key -out server.csr -subj "/CN=$VIRTUAL_HOST"
-openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
-cp server.crt /etc/cert/cert.crt
-cp server.key /etc/cert/cert.key
+openssl req -newkey rsa:2048 -sha256 -nodes -keyout YOURPRIVATE.key -x509 -days 365 -out YOURPUBLIC.pem -subj "/CN=$VIRTUAL_HOST"
+
+cp YOURPUBLIC.pem /etc/cert/cert.pem
+cp YOURPRIVATE.key /etc/cert/cert.key
 nginx -g "daemon off;"
